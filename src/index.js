d9198d3b1657f2983f64454c7b857e7a92562cc2
Mark Freeman <dorsettri@gmail.com>
2021-02-20 14:12:29 +0000

    remove inline css, trim to one component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';  
 import ReactDom from 'react-dom'; 
 
+// CSS
 import './index.css'; 
 
 function Booklist(){
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
     </section>
   );
@@ -20,16 +15,15 @@ function Booklist(){
 // building Components
 const Book = () => {
   return (
-    <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <article className='book'>
+        <img
+          src="https://m.media-amazon.com/images/I/51oZCDb0JNL._AC_UL320_.jpg"
+          alt="road to react book cover"
+        />
+      <h1>The Road to React: Your journey to master plain yet pragmatic React.js</h1>
+      <h4>Robin Wieruch</h4>
     </article>
   );
 }
-
-const Image = () => <img src="https://m.media-amazon.com/images/I/51oZCDb0JNL._AC_UL320_.jpg" alt="road to react book cover"/>
-const Title = () => <h1>The Road to React: Your journey to master plain yet pragmatic React.js</h1>;
-const Author = () => <h4>Robin Wieruch</h4>
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
